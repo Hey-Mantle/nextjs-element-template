4683e562139a28671e9ecd71e7cbd6dda51a65a6
Kristian PD <kristianpd@gmail.com>
2025-09-18 13:02:40 -0400

    migration rollup into single init

--- prisma/migrations/20250911204434_add_organization_fields_to_user/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "users" ADD COLUMN "organizationId" TEXT;
-ALTER TABLE "users" ADD COLUMN "organizationName" TEXT;
--- prisma/migrations/20250916211513_add_api_token_to_organization/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `organizationId` on the `users` table. All the data in the column will be lost.
-  - A unique constraint covering the columns `[userId,organizationId]` on the table `user_organizations` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- AlterTable
-ALTER TABLE "organizations" ADD COLUMN "apiToken" TEXT;
