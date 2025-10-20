e920d8988da0a176291e1948f79c0957196eeb8b
Kristian PD <kristianpd@gmail.com>
2025-10-20 15:00:33 -0400

    Rollup init migration

--- prisma/migrations/20251020190016_init/migration.sql
+++ prisma/migrations/20251020190016_init/migration.sql
@@ -32,6 +32,22 @@ CREATE TABLE "user_organizations" (
     CONSTRAINT "user_organizations_organizationId_fkey" FOREIGN KEY ("organizationId") REFERENCES "organizations" ("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
+-- CreateTable
+CREATE TABLE "user_access_tokens" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "userId" TEXT NOT NULL,
+    "organizationId" TEXT NOT NULL,
+    "accessToken" TEXT NOT NULL,
+    "tokenType" TEXT NOT NULL,
+    "scope" TEXT NOT NULL,
+    "expiresAt" DATETIME,
+    "refreshToken" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "user_access_tokens_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "user_access_tokens_organizationId_fkey" FOREIGN KEY ("organizationId") REFERENCES "organizations" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
 
@@ -40,3 +56,6 @@ CREATE UNIQUE INDEX "organizations_mantleId_key" ON "organizations"("mantleId");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "user_organizations_userId_organizationId_key" ON "user_organizations"("userId", "organizationId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_access_tokens_userId_organizationId_tokenType_key" ON "user_access_tokens"("userId", "organizationId", "tokenType");
